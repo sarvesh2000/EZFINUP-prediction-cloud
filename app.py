91d1696a157883e221c41a1cd25fbca8ed6e4f39
Sarvesh <sarvesh4232@gmail.com>
2021-01-22 22:42:32 +0530

    Added CORS

--- app.py
+++ app.py
@@ -16,10 +16,10 @@ app = Flask(__name__)
 CORS(app)
 
 @app.after_request
-    def after_request(response):
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-        return response
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    return response
 
 @app.route('/')
 def index():
